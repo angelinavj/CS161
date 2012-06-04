39b91b5f30122005141e9a27ee861cc74edb5021
angelinavj <angelina.veni@gmail.com>
2012-06-03 17:14:47 -0700

    Add test files

---
+++ compare_slow_fast.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: ./compare_slow_fast.sh CPPSOURCEFILE CPPSOURCEFILE"
+  exit
+fi
+
+echo "Compiling and running the C++ file $1..."
+rm big.out.res
+g++ -o source $1 && ./source < big.in > big.out.res
+rm random_length.out.res
+g++ -o source $1 && ./source < random_length.in > random_length.out.res
+
+
+rm big.out.ref
+g++ -o source $2 && ./source < big.in > big.out.ref
+rm random_length.out.ref
+g++ -o source $2 && ./source < random_length.in > random_length.out.ref
+
+diff big.out.res big.out.ref
+diff random_length.out.res random_length.out.ref
+echo "If user time + system time < 10 seconds, you should be fine for 
+the real test."
